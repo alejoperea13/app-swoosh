cd04845e16f6400c0a4099c2e4aafe7911eeaa85
Alejandro perea <Alejandro@Alejandros-MacBook-Pro.local>
2017-12-09 02:02:07 -0500

    screens done with constrains

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -16,8 +16,11 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+    }
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
         
-
     }
 
 
